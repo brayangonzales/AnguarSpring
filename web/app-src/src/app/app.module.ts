f73c4486d3a13b96a50a9d3995ef66e6f60b110f
brayan <picaro910@gmail.com>
2019-03-23 15:06:54 -0400

    agregando nuevo componente

--- web/app-src/src/app/app.module.ts
+++ web/app-src/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,HeaderComponent]
 })
 export class AppModule { }
